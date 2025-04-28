c79501a2ec15b4cb652e16c34dda94f55bf3269d
ramsvikyolo <49155620+ramsvikyolo@users.noreply.github.com>
2025-04-28 03:51:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,8 @@
 
 # Replace this with your real Bot Token from BotFather
 TELEGRAM_TOKEN = "8015540396:AAHGX0kwF5jpXcm4d06nE01_zPNs1lvBgwA"
-
 # Your Telegram user ID (for admin-only functions)
-ADMIN_ID = 7887093055  # Replace with your Telegram numeric ID
+ADMIN_ID = 8155680856  # Replace with your Telegram numeric ID
 
 # Basic Settings
 WELCOME_MESSAGE = "Welcome to the group! Please be respectful."
