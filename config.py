fad4ec90712690a7817289cd090659af527d7bb7
ramsvikyolo <49155620+ramsvikyolo@users.noreply.github.com>
2025-05-01 17:23:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Replace this with your real Bot Token from BotFather
-TELEGRAM_TOKEN = "8015540396:AAHGX0kwF5jpXcm4d06nE01_zPNs1lvBgwA"
+TELEGRAM_TOKEN = "7426054193:AAHmVG5mOaqtSfbR-NV1BuPYTwe-0xulVns"
 # Your Telegram user ID (for admin-only functions)
 ADMIN_ID = 8155680856  # Replace with your Telegram numeric ID
 
